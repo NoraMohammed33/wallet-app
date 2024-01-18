e17c4da9044b78cb9666fafe18f79664d05b644e
Nora Mohammed <NoorMohammed3399@gmail.com>
2024-01-18 14:45:01 +0200

    add wallet component and add bootstarp to project

---
+++ src/components/Wallet/Wallet.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from './Wallet.module.css'
+
+export default function Wallet() {
+    return (
+        <>
+            <input className='form-control w-25'/>
+        </>
+    );
+}
